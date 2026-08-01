2b82f5e52d1ab0aeb73b0901d1ee48c7047b6553
Nikunj Raval <rockeydon201@gmail.com>
2026-08-02 00:41:54 +0530

    changes in app file

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://adminsouthdelight.netlify.app/",
-    "https://gloriousdays.netlify.app/"
+    "https://adminsouthdelight.netlify.app",
+    "https://gloriousdays.netlify.app"
   ],
   credentials: true
 }));
